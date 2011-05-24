b3262bda9f1aad3a6c1c308e23f824836e227723
Jonathan Anderson <jonathan.anderson@ieee.org>
2011-05-24 00:56:55 -0230

    Add libcwrap, a wrapper around libc's open().
    This libc wrapper allows code to prepare caches of file descriptors which, when open() is called, will be searched in preference to actually performing the open() system call.

---
+++ cwrap/open.c
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2011 Jonathan Anderson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <dlfcn.h>
+#include <stdarg.h>
+#include <string.h>
+
+#include <sys/capability.h>
+
+#include <libcapsicum.h>
+
+#include "capmode.h"
+
+
+//! Signature of the libc open(2) function.
+typedef int (*open_signature)(const char*, int, ...);
+
+
+//! FDList-aware wrapper around libc's open().
+int open(const char *path, int flags, ...)
+{
+	static open_signature unwrapped = 0;
+	if (unwrapped == 0)
+		unwrapped = (open_signature) dlsym(RTLD_NEXT, "open");
+
+	va_list ap;
+	va_start(ap, flags);
+
+	struct lc_fdlist *fds = lc_fdlist_global();
+
+	// If there is no global FD list, fall back to libc.
+	if (fds == 0)
+		return unwrapped(path, flags, ap);
+
+	// Try to find the file.
+	const char *relative_name;
+	int fd = lc_fdlist_find(fds, "org.freebsd.capsicum.capsh", "files", path,
+			&relative_name);
+
+	if (fd >= 0) {
+		// Perhaps we found a directory descriptor and relative filename?
+		if (strnlen(relative_name, 1) != 0)
+			fd = openat(fd, relative_name, flags, ap);
+
+		return fd;
+	}
+
+	// If we failed to find the file, and we're not in capability mode, fall
+	// through to libc's open().
+	if (!in_capability_mode())
+		return unwrapped(path, flags, ap);
+
+	// We're in capability mode, so we can't fall back to libc.
+	// We have a list of files, but the one we're looking for isn't in it.
+	// Return error, with errno set by lc_fdlist_find().
+	return (-1);
+}
+
---
+++ cwrap/test.c
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2011 Jonathan Anderson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <sys/mman.h>
+
+#include <err.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+#include <libcapsicum.h>
+
+
+int main(int argc, char *argv[])
+{
+	const char *filename = "/etc/passwd";
+
+	// Open the descriptor normally (this should fall through).
+	int fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		err(-1, "open('%s')", filename);
+
+	// Store it in a descriptor list.
+	struct lc_fdlist *fds = lc_fdlist_new();
+	if (0 != lc_fdlist_addcap(fds,
+			"org.freebsd.capsicum.capsh", "file", filename,
+			fd, CAP_READ | CAP_SEEK))
+		err(-1, "lc_fdlist_addcap()");
+
+	// A place to store the FD list.
+	//
+	// TODO: this should move to after the cap_enter() call once shm_open()
+	//       is allowed in capability mode with SHM_ANON.
+	int shm = shm_open(SHM_ANON, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (shm < 0) err(-1, "shm_open(SHM_ANON)");
+
+
+	// Enter capability mode - further calls to open() should fail.
+	cap_enter();
+
+	// This open() call should fail; we are in capability mode.
+	if (open(filename, O_RDONLY) != -1)
+		errx(-1, "open('%s') succeeded in capability mode", filename);
+
+	// Save the descriptor list as the default, global one.
+	if (0 != lc_fdlist_set_global(fds, shm))
+		err(-1, "lc_fdlist_set_global()");
+
+	// Now open() should work, because the descriptor has been cached.
+	int copy = open(filename, O_RDONLY);
+	if (copy < 0)
+		err(-1, "open('%s')", filename);
+
+	// Everything works!
+	printf("OK\n");
+
+	return 0;
+}
+
